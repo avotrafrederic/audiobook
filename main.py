3aa4b209ff531b3e9e5af4de06eefe5504c21332
ZandryK <avotrafredericrak@gmail.com>
2023-10-19 10:27:33 +0300

    update script

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #-*- encoding:UTF-8 -*-
-#! env/Scripts/python
+#! /usr/bin/puthon
 from include.Pdf import PDF
-filename = input("Entrez le fichier pdf: ")
-voice = int(input('Veuillez choisir le genre de voi que vous voulez [masculine (0) | feminine (1)]: '))
-PDF.main(fileName=filename,voice=voice)
+import pyfiglet
+import colored
+import subprocess
+def main():
+    subprocess.run("clear",shell=True)
+    title = pyfiglet.figlet_format("PDF AUDIOBOOK")
+    print(f"{colored.fg(37)} {title}".center(20))
+    filename = input(f"{colored.fg(6)}Entrez le fichier pdf: ")
+    voice = int(input('Veuillez choisir le genre de voi que vous voulez [masculine (0) | feminine (1)]: '))
+    PDF.main(fileName=filename,voice=voice)
+
+if __name__ == "__main__":
+    main()
